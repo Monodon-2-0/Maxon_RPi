d602fde3c3971bada9e2d7258372ad2cc34f43f8
germanmonodon <german@monodon.es>
2023-12-18 17:29:24 +0100

    Example camera servo

--- Maxon_control_asynchronous.py
+++ Maxon_control_asynchronous.py
@@ -70,6 +70,10 @@ def get_position_is(node_n):
 def move_to_position_speed(target_position, target_speed, node_n, servo_port):
     internal_variable = 0
     print("Moving to the reference proposed....")
+    us_camera = 1100
+    us_tail = 1200
+    back = 0
+    back_c = 0
     while True:
         if target_speed != 0:
             epos.VCS_SetPositionProfile(keyHandle, node_n, target_speed, acceleration, deceleration,
@@ -90,6 +94,28 @@ def move_to_position_speed(target_position, target_speed, node_n, servo_port):
         us2 = 2200 - (us - 800)
         set_servo_pwm(servo_port, us2)
         set_servo_pwm(13, us)
+
+        # Implementar que gire la camara y el servo de atras
+        if us_tail == 1400:
+            back = 1
+        elif us_tail == 1200:
+            back = 0
+        if back == 1:
+            us_tail = us_tail - 20
+        else:
+            us_tail = us_tail + 20
+        if us_camera == 1900:
+            back_c = 1
+        elif us_camera == 1100:
+            back_c = 0
+        if back_c == 1:
+            us_camera = us_camera - 20
+        else:
+            us_camera = us_camera + 20
+
+        set_servo_pwm(15, us_tail)
+        set_servo_pwm(10, us_camera)
+        set_servo_pwm(10, us)
         print(f"microsegundos de pwm {us} y posicion del motor {get_position_is(node_n)}", )
         if true_position == target_position:
             break
@@ -121,6 +147,7 @@ if __name__ == "__main__":
     for s in range(2):
         move_to_position_speed(10000, 400, 1, 8)
         move_to_position_speed(-10000, 400, 1, 8)
+
         # move to position 0 steps at 2000 rpm/s
 
     move_to_position_speed(0, 200, 1, 8)  # move to position 0 steps at 2000 rpm/s
